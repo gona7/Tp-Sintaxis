09a65cb8ee292ce75d1dc240a6d1c159ee3758e6
gona7 <86001419+gona7@users.noreply.github.com>
2021-09-01 09:59:47 -0300

    Add files via upload

---
+++ scanner.c
@@ -0,0 +1,66 @@
+#include "scanner.h"
+
+char c;
+
+void get_token()
+{
+    TOKEN token;
+    char carac;
+    do
+    {
+    char *buffer;
+    int i = 0;
+    c = getchar();
+    carac = que_caracter_es();
+    switch(carac){
+    case ALGO:
+        while(carac == ALGO){
+        buffer[i] = c;
+        i++;
+        c = getchar();
+        carac = que_caracter_es();
+        }
+        buffer[i] = '\0';
+        ungetc(c,stdin);
+        token = CADENA;
+        break;
+    case COMA:
+        ungetc(c,stdin);
+        token = SEPARADOR;
+        break;
+    case ESP:
+        ungetc(c,stdin);
+        token = ESPACIO;
+        break;
+    case FDT:
+        ungetc(c,stdin);
+        token = FIN_DE_TEXTO;
+        break;
+    }
+    switch(token) {
+        case CADENA:
+        printf("Cadena: %d\n", buffer);
+        break;
+        case SEPARADOR:
+        printf("Separador: ,\n");
+        break;
+        case ESPACIO:
+        break;
+    }
+    } while(token != FIN_DE_TEXTO);
+    if(token == FIN_DE_TEXTO)
+        printf("Fin de texto: \n");
+}
+
+
+CARACTER que_caracter_es()
+{
+    if(isspace(c))
+        return ESP;
+    else if(c == ',')
+        return COMA;
+    else if(c == EOF)
+        return FDT;
+    else
+        return ALGO;
+}
