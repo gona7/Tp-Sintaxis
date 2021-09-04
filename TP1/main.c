589c4869aa086e88a737b0ea82a13087f35721f5
gona7 <86001419+gona7@users.noreply.github.com>
2021-09-04 17:07:35 -0300

    Add files via upload

---
+++ TP1/main.c
@@ -0,0 +1,26 @@
+#include "scanner.h"
+#include <stdlib.h>
+
+int main(void)
+{
+    TOKEN token;
+    char *cad[100];
+    do
+    {
+        token = get_token(cad);
+        switch(token) {
+            case CADENA:
+            *cad =
+            printf("Cadena: %s\n", cad);
+            break;
+            case SEPARADOR:
+            printf("Separador: ,\n");
+            break;
+            case ESPACIO:
+            break;
+        }
+    } while(token != FIN_DE_TEXTO);
+    printf("Fin de texto: \n");
+    return EXIT_SUCCESS;
+}
+
---
+++ TP1/scanner.c
@@ -0,0 +1,53 @@
+#include "scanner.h"
+
+char c;
+
+TOKEN get_token(char *buffer)
+{
+    TOKEN tk;
+    CARACTER carac;
+    do
+    {
+    char *buffer[100];
+    int i = 0;
+    c = getchar();
+    carac = que_caracter_es();
+    switch(carac){
+    case ALGO:
+        while(carac == ALGO){
+        buffer[i] = c;
+        i++;
+        c = getchar();
+        carac = que_caracter_es();
+        }
+        buffer[i] = '\0';
+        ungetc(c,stdin);
+        tk = CADENA;
+        return tk;
+    case COMA:
+        ungetc(c,stdin);
+        tk = SEPARADOR;
+        return tk;
+    case ESP:
+        ungetc(c,stdin);
+        tk = ESPACIO;
+        return tk;
+    }
+    } while(carac != FDT);
+    tk = FIN_DE_TEXTO;
+    return tk;
+
+}
+
+
+    CARACTER que_caracter_es()
+{
+    if(isspace(c))
+        return ESP;
+    else if(c == ',')
+        return COMA;
+    else if(c == EOF)
+        return FDT;
+    else
+        return ALGO;
+}
